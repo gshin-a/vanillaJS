06e6a4edc26eed3b9434f1f47de419ad6da01ab3
ghshin-a <singahyeon12@naver.com>
2023-06-02 15:47:03 +0900

    feat: 시계 완성

--- momentum/clock.js
+++ momentum/clock.js
@@ -1,18 +1,22 @@
 const clockContainer = document.querySelector(".js-clock"),
   clockTitle = clockContainer.querySelector("h1");
 
+function refactorDate(time) {
+  return time < 10 ? `0${time}` : time;
+}
+
 function getTime() {
   const date = new Date();
   const minutes = date.getMinutes();
   const hours = date.getHours();
   const seconds = date.getSeconds();
-  clockTitle.innerText = `${hours}:${minutes}:${seconds}`;
+  clockTitle.innerText = `${refactorDate(hours)}:${refactorDate(
+    minutes
+  )}:${refactorDate(seconds)}`;
 }
 
 function init() {
-  setInterval(() => {
-    getTime();
-  }, 1000);
+  setInterval(getTime, 1000);
 }
 
 init();
