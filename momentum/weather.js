19bd99448f4b21282d7135aa1e4f2e175a7d2685
ghshin-a <singahyeon12@naver.com>
2023-06-07 14:48:59 +0900

    api 수정

--- momentum/weather.js
+++ momentum/weather.js
@@ -1,32 +1,37 @@
 const xhr = new XMLHttpRequest();
 xhr.open("GET", ".env", true);
 xhr.onload = function () {
+  let apiKey = -1;
   if (xhr.status === 200) {
     const envVariables = parseEnvFile(xhr.responseText);
     // envVariables 객체를 사용하여 환경 변수에 접근할 수 있습니다.
-    const apiKey = envVariables.API_KEY;
+    apiKey = envVariables.API_KEY;
     // 환경 변수를 사용하는 코드를 작성합니다.
-    function onGeoOk(position) {
-      const lat = position.coords.latitude;
-      const lon = position.coords.longitude;
-      const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
-      fetch(url)
-        .then((res) => res.json())
-        .then((data) => {
-          const city = document.querySelector("#weather span:first-child");
-          const weather = document.querySelector("#weather span:nth-child(2)");
-          const temp = document.querySelector("#weather span:last-child");
-          city.innerText = `${data.name} .`;
-          weather.innerText = `${data.weather[0].main} .`;
-          temp.innerText = `${Math.round(data.main.temp - 273.15)}°C`;
-        });
-    }
-    function onGeoError() {
-      alert("Can't find you. No weather for you.");
-    }
-
-    navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
+    console.log("envVariables");
+  } else {
+    apiKey = API_KEY;
+    console.log("API_KEY");
   }
+  function onGeoOk(position) {
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        const city = document.querySelector("#weather span:first-child");
+        const weather = document.querySelector("#weather span:nth-child(2)");
+        const temp = document.querySelector("#weather span:last-child");
+        city.innerText = `${data.name} .`;
+        weather.innerText = `${data.weather[0].main} .`;
+        temp.innerText = `${Math.round(data.main.temp - 273.15)}°C`;
+      });
+  }
+  function onGeoError() {
+    alert("Can't find you. No weather for you.");
+  }
+
+  navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
 };
 xhr.send();
 
