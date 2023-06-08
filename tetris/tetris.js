b1cf1d4195827318c824e837fdd35d4bd6f0aad4
ghshin-a <singahyeon12@naver.com>
2023-06-08 17:58:50 +0900

    테트리스 만드는 중

--- tetris/tetris.js
+++ tetris/tetris.js
@@ -1,146 +1,153 @@
 const gameView = document.querySelector("#game-view");
 
-const viewList = Array(20)
-  .fill(0)
-  .map(() => Array(12).fill(0));
-
-const blockList = {
-  0: [
-    [0, 5],
-    [0, 6],
-    [0, 7],
-    [0, 8],
-  ],
-  1: [
-    [1, 5],
-    [1, 6],
-    [1, 7],
-    [0, 6],
-  ],
-  2: [
-    [1, 5],
-    [1, 6],
-    [0, 6],
-    [0, 7],
-  ],
-  3: [
-    [1, 5],
-    [1, 6],
-    [0, 5],
-    [0, 6],
-  ],
-  4: [
-    [1, 5],
-    [1, 6],
-    [1, 7],
-    [0, 7],
-  ],
-  5: [
-    [1, 5],
-    [1, 6],
-    [1, 7],
-    [0, 5],
-  ],
-  6: [
-    [1, 6],
-    [1, 7],
-    [0, 5],
-    [0, 6],
-  ],
+let blockList = Array(240).fill(0);
+const blockType = {
+  0: [5, 6, 7, 8],
+  1: [17, 18, 19, 6],
+  2: [17, 18, 6, 7],
+  3: [17, 18, 5, 6],
+  4: [17, 18, 19, 7],
+  5: [17, 18, 19, 5],
+  6: [18, 19, 5, 6],
 };
-
-let activeBlock = blockList[Math.floor(Math.random() * 7)];
+let activeBlock = blockType[Math.floor(Math.random() * 7)];
 let filledBlock = [];
-let validation = Array(20 * 12).fill(0);
+let validation = Array(240).fill(0);
 
-// activeBlock과 filledBlock은 1(색칠), 나머지는 0으로 view를 보여줌
-function renderGameView(viewList) {
-  gameView.innerHTML = "";
-  for (let i = 0; i < 20; i++) {
-    for (let j = 0; j < 12; j++) {
-      viewList[i][j] = 0;
-    }
-  }
-  activeBlock.forEach((e) => (viewList[e[0]][e[1]] = 1));
-  filledBlock.forEach((e) => e.forEach((e) => (viewList[e[0]][e[1]] = 1)));
-  viewList.forEach((e) =>
-    e.forEach((e) => {
-      const item = document.createElement("div");
-      item.id = "item";
-      if (e === 1) {
-        item.classList.add("active");
-      }
-      gameView.appendChild(item);
-    })
-  );
+// 초기 게임 화면 세팅
+function startGame() {
+  blockList.forEach((e, i) => {
+    const item = document.createElement("div");
+    item.id = `item${i}`;
+    item.classList.add("item");
+    gameView.appendChild(item);
+  });
 }
 
-// 방향키에 따라 acitveBlock 위치 조정하고 renderGameView 함수 실행
-function keyDownHandler(e) {
-  if (validateBlock(activeBlock)) {
-    // clearInterval(interval);
-    switchActiveBlock();
-    return;
+// acitiveBlock 보여줌
+function renderActiveBlock() {
+  blockList.forEach((e, i) => {
+    const item = document.querySelector(`#item${i}`);
+    if (e === 0 && item.classList.contains("active"))
+      item.classList.remove("active");
+  });
+
+  activeBlock.forEach((e) => {
+    const item = document.querySelector(`#item${e}`);
+    if (!item.classList.contains("active")) item.classList.add("active");
+  });
+}
+
+// 예정 방향으로 이동할 수 있는지 확인 (게임 화면을 벗어나거나 해당 위치에 블럭이 존재하면 이동 불가능)
+// type: left, right, down
+function validate(type) {
+  let isFloor = 0;
+  switch (type) {
+    case "left":
+      activeBlock.forEach(
+        (e) => (isFloor += e % 12 === 0 || blockList[e - 1] === 1)
+      );
+      break;
+    case "right":
+      activeBlock.forEach(
+        (e) => (isFloor += e % 12 === 11 || blockList[e + 1] === 1)
+      );
+      break;
+    case "down":
+      activeBlock.forEach(
+        (e) => (isFloor += Math.floor(e / 12) === 19 || blockList[e + 12] === 1)
+      );
   }
+
+  return isFloor;
+}
+
+function renderGameView() {
+  blockList.forEach((e, i) => {
+    const item = document.querySelector(`#item${i}`);
+    if (e === 1) item.classList.add("active");
+  });
+}
+
+// activeBlock이 아래로 이동할 수 없으면 activeBlock을 blockList에 저장하고 다음 activeBlock 생성
+// 게임 화면 재렌더링
+function switchActiveBlock() {
+  activeBlock.forEach((e) => (blockList[e] = 1));
+  activeBlock = JSON.parse(
+    JSON.stringify(blockType[Math.floor(Math.random() * 7)])
+  );
+  renderGameView();
+}
+
+function clearRow() {
+  const sumRows = Array(20).fill(0);
+  blockList.forEach((e, i) => {
+    sumRows[Math.floor(i / 12)] += e;
+  });
+
+  let clearRows = [];
+  sumRows.forEach((e, i) => {
+    if (e === 12) {
+      clearRows.push(i);
+    }
+  });
+  clearRows.sort((a, b) => b - a);
+  clearRows.forEach((e) => {
+    blockList.splice(e * 12, 12);
+  });
+  const newRow = Array(12).fill(0);
+  for (let i = 0; i < clearRows.length; i++) {
+    blockList.unshift(...newRow);
+  }
+}
+
+// 방향키 event
+function keyDownHandler(e) {
   if (e.key === 37 || e.key === "ArrowRight") {
-    for (let i = 0; i < activeBlock.length; i++) {
-      activeBlock[i][1]++;
+    if (validate("right")) {
+    } else {
+      for (let i = 0; i < activeBlock.length; i++) {
+        activeBlock[i]++;
+      }
     }
   } else if (e.key === 39 || e.key === "ArrowLeft") {
-    for (let i = 0; i < activeBlock.length; i++) {
-      activeBlock[i][1]--;
+    if (validate("left")) {
+    } else {
+      for (let i = 0; i < activeBlock.length; i++) {
+        activeBlock[i]--;
+      }
     }
   } else if (e.key === 38 || e.key === "ArrowUp") {
     for (let i = 0; i < activeBlock.length; i++) {
-      activeBlock[i][0]--;
+      activeBlock[i] -= 12;
     }
   } else if (e.key === 40 || e.key === "ArrowDown") {
-    for (let i = 0; i < activeBlock.length; i++) {
-      activeBlock[i][0]++;
+    if (validate("down")) {
+      switchActiveBlock();
+    } else {
+      for (let i = 0; i < activeBlock.length; i++) {
+        activeBlock[i] += 12;
+      }
     }
   }
-  renderGameView(viewList);
-}
-
-// 맨 처음에 view를 보여줌
-function startGame() {
-  renderGameView(viewList);
-}
-
-// 기존의 acitiveBlock이 바닥에 닿아 이동 종료되고, 새로운 acitiveBlock이 생성됨
-function switchActiveBlock() {
-  activeBlock.forEach((e) => (validation[e[0] * 12 + e[1]] = 1));
-  filledBlock.push(activeBlock);
-
-  activeBlock = JSON.parse(
-    JSON.stringify(blockList[Math.floor(Math.random() * 7)])
-  );
-}
-
-// 입력된 block의 위치가 valition이 1이거나
-// block의 y축 위치가 18을 넘으면 false 반환
-// validation과 비교하는 방법: block 위치에서 first*12 + second === validation의 index
-function validateBlock() {
-  let isValid = 0;
-  if (activeBlock[0][0] > 18) return (isValid = 1);
-  activeBlock.forEach((e) => {
-    if (validation[(e[0] + 1) * 12 + e[1]]) {
-      return (isValid = 1);
-    }
-  });
-  return isValid;
+  clearRow();
+  renderActiveBlock();
 }
 
 function init() {
   startGame();
+  renderActiveBlock();
   let interval = setInterval(() => {
-    if (validateBlock(activeBlock)) {
+    if (validate("down")) {
       switchActiveBlock();
+      clearRow();
+      renderActiveBlock();
     } else {
       for (let i = 0; i < activeBlock.length; i++) {
-        activeBlock[i][0]++;
+        activeBlock[i] += 12;
       }
-      renderGameView(viewList, interval);
+      clearRow();
+      renderActiveBlock();
     }
   }, 1000);
 }
