b4ccb96625b32dda02b5c33e236759387aa02409
ghshin-a <singahyeon12@naver.com>
2023-06-09 15:27:06 +0900

    feat: 점수, 레벨 기능 추가 & 레벨 올라가면 0.05초 빨라짐

--- tetris/tetris.js
+++ tetris/tetris.js
@@ -1,4 +1,6 @@
 const gameView = document.querySelector("#game-view");
+const scoreView = document.querySelector("#score div:nth-child(2)");
+const levelView = document.querySelector("#level div:nth-child(2)");
 
 let blockList = Array(240).fill(0);
 const blockType = {
@@ -13,34 +15,40 @@ const blockType = {
 let activeBlock = blockType[Math.floor(Math.random() * 7)];
 let filledBlock = [];
 let validation = Array(240).fill(0);
+let score = 0;
+let level = 1;
 
 // 초기 게임 화면 세팅
 function startGame() {
   blockList.forEach((e, i) => {
-    const item = document.createElement("div");
+    let item = document.createElement("div");
     item.id = `item${i}`;
     item.classList.add("item");
     gameView.appendChild(item);
   });
+
+  scoreView.innerText = score;
+  levelView.innerText = level;
 }
 
 // acitiveBlock 보여줌
 function renderActiveBlock() {
   blockList.forEach((e, i) => {
-    const item = document.querySelector(`#item${i}`);
+    let item = document.querySelector(`#item${i}`);
     if (e === 0 && item.classList.contains("active"))
       item.classList.remove("active");
   });
 
   activeBlock.forEach((e) => {
-    const item = document.querySelector(`#item${e}`);
+    let item = document.querySelector(`#item${e}`);
     if (!item.classList.contains("active")) item.classList.add("active");
   });
 }
 
 // 예정 방향으로 이동할 수 있는지 확인 (게임 화면을 벗어나거나 해당 위치에 블럭이 존재하면 이동 불가능)
-// type: left, right, down
-function validate(type) {
+// type: left, right, down, rotate
+// rotate는 인자값으로 들어오는 위치가 유효한지 확인
+function validate(type, rotatedActiveBlock) {
   let isFloor = 0;
   switch (type) {
     case "left":
@@ -57,6 +65,10 @@ function validate(type) {
       activeBlock.forEach(
         (e) => (isFloor += Math.floor(e / 12) === 19 || blockList[e + 12] === 1)
       );
+      break;
+    case "rotate":
+      rotatedActiveBlock.forEach((e) => (isFloor += blockList[e] === 1));
+      break;
   }
 
   return isFloor;
@@ -64,8 +76,7 @@ function validate(type) {
 
 function renderGameView() {
   blockList.forEach((e, i) => {
-    const item = document.querySelector(`#item${i}`);
-    if (e === 1) item.classList.add("active");
+    if (e === 1) document.querySelector(`#item${i}`).classList.add("active");
   });
 }
 
@@ -79,6 +90,28 @@ function switchActiveBlock() {
   renderGameView();
 }
 
+function addScore() {
+  score += 10;
+  scoreView.innerText = score;
+  level = Math.floor(score / 50) + 1;
+  levelView.innerText = level;
+  clearInterval(interval);
+
+  interval = setInterval(() => {
+    if (validate("down")) {
+      switchActiveBlock();
+      clearRow();
+      renderActiveBlock();
+    } else {
+      for (let i = 0; i < activeBlock.length; i++) {
+        activeBlock[i] += 12;
+      }
+      clearRow();
+      renderActiveBlock();
+    }
+  }, 1000 - (level - 1) * 50);
+}
+
 function clearRow() {
   const sumRows = Array(20).fill(0);
   blockList.forEach((e, i) => {
@@ -86,18 +119,22 @@ function clearRow() {
   });
 
   let clearRows = [];
+
   sumRows.forEach((e, i) => {
     if (e === 12) {
       clearRows.push(i);
     }
   });
-  clearRows.sort((a, b) => b - a);
-  clearRows.forEach((e) => {
-    blockList.splice(e * 12, 12);
-  });
-  const newRow = Array(12).fill(0);
-  for (let i = 0; i < clearRows.length; i++) {
-    blockList.unshift(...newRow);
+  if (clearRows.length > 0) {
+    clearRows.sort((a, b) => b - a);
+    clearRows.forEach((e) => {
+      blockList.splice(e * 12, 12);
+      addScore();
+    });
+    const newRow = Array(12).fill(0);
+    for (let i = 0; i < clearRows.length; i++) {
+      blockList.unshift(...newRow);
+    }
   }
 }
 
@@ -122,12 +159,13 @@ function rotateBlock() {
     }
 
     let rotation = activeBlock[i] + (-11 * horDist + 13 * perDist);
+
     if (rotation < 0 || rotation > 239) {
       disable = 1;
     }
     rotatedActiveBlock.push(rotation);
   });
-  if (!disable) {
+  if (!disable && !validate("rotate", rotatedActiveBlock)) {
     activeBlock = JSON.parse(JSON.stringify(rotatedActiveBlock));
   }
 }
@@ -163,10 +201,12 @@ function keyDownHandler(e) {
   renderActiveBlock();
 }
 
+let interval = null;
+
 function init() {
   startGame();
   renderActiveBlock();
-  let interval = setInterval(() => {
+  interval = setInterval(() => {
     if (validate("down")) {
       switchActiveBlock();
       clearRow();
